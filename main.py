a965f72c49b7af5911f3ff3a337bedf51186d81c
Taeke d <95484190+Taeke06@users.noreply.github.com>
2022-06-10 09:55:12 +0000

    computer kiest willekeurig woord

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import random
 #welkomstekst
 print("Welkom bij Galgje!\nGalgje is een spel waar je een woord moet raden die de computer hier heeft uitgekozen.")
 
 #woordenlijst
 woordenlijst= ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit" , "heesterperk"]
+
+#computer kiest willekeurig woord
+woord= random.choice (woordenlijst)
+
