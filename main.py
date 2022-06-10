7afcef0a3f98e6895b747f7f918c75a8bf81b1c1
Taeke d <95484190+Taeke06@users.noreply.github.com>
2022-06-10 09:50:28 +0000

    woordenlijst

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 #welkomstekst
 print("Welkom bij Galgje!\nGalgje is een spel waar je een woord moet raden die de computer hier heeft uitgekozen.")
+
+#woordenlijst
+woordenlijst= ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit" , "heesterperk"]
